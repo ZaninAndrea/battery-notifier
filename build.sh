09e79a0ed94b727948cf9a21f32a4149d01b0c87
Andrea Zanin <99.zanin@gmail.com>
2022-01-24 17:54:35 +0100

    new icon

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+rm -rf ./releases
+mkdir ./releases
+mkdir ./releases/linux ./releases/macos-apple ./releases/macos-intel ./releases/windows
+cp -r "./Battery Notifier Template.app" "./releases/macos-apple/Battery Notifier.app/"
+cp -r "./Battery Notifier Template.app" "./releases/macos-intel/Battery Notifier.app/"
+
 env GOOS=darwin GOARCH=arm64 go build -o "./releases/macos-apple/Battery Notifier.app/Contents/MacOS/notifier" .
 env GOOS=darwin GOARCH=amd64 go build -o "./releases/macos-intel/Battery Notifier.app/Contents/MacOS/notifier" .
 env GOOS=windows GOARCH=amd64 go build -o "./releases/windows/notifier.exe" .
